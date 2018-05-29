e33ed7086af6e75551dc1a8092ace7b130d81355
Filip Ginter <ginter@cs.utu.fi>
2018-05-29 12:00:49 +0300

    lemmatizer

--- lemmatizer_server.py
+++ lemmatizer_server.py
@@ -31,19 +31,13 @@ class LemmatizerWrapper():
         result_conllu=self.lemmatizer_model.lemmatize_batch(conllu)
         return result_conllu
 
-def launch(args):
-    handler_class=LemmatizerHTTPDummyHandler
+def launch(args,q_in,q_out):
     lemmatizer=LemmatizerWrapper(args)
-    handler_class.parser=lemmatizer
-    httpd = http.server.HTTPServer(("",args.port), handler_class)
-    httpd.serve_forever()
+    while True:
+        txt=q_in.recv()
+        q_out.send(lemmatizer.parse_text(txt))
 
 argparser = argparse.ArgumentParser(description='Lemmatize conllu text')
-argparser.add_argument('--port', metavar='TCP port', type=int, default=32787, help='port')
 argparser.add_argument('--model', default='models/lemmatizer.pt', type=str, help='Model')
 argparser.add_argument('--gpu', type=int, default=-1, help='Gpu device id, if -1 use cpu')
 argparser.add_argument('--batch_size', type=int, default=64, help='Batch size')
-
-if __name__=="__main__":
-    args=argparser.parse_args()
-    launch(args)
