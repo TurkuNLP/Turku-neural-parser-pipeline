13c2400fc99f8558cf6bfbf2013b6b971abdd174
Filip Ginter <ginter@cs.utu.fi>
2018-05-31 16:18:30 +0300

    latest tokenizer

--- tokenizer_server.py
+++ tokenizer_server.py
@@ -10,39 +10,16 @@ class TokenizerWrapper():
         """
         Tokenizer model loading etc goes here
         """
-
-        if args.model:
-            self.model = load_model(args.model)
-        else:
-            self.model = load_model('model')
-        if args.vocab:
-            inf = open(args.vocab,'rb')
-        else:
-            inf = open('vocab.pickle','rb')
-        self.vocab = pickle.load(inf)
-        inf.close()
+        from keras.models import load_model
+        self.model = load_model(args.model)
+        with open(args.vocab,'rb') as inf:
+            self.vocab = pickle.load(inf)
             
     def parse_text(self,txt):
-        buff=io.StringIO()
-        x = []
-        for c in txt.replace('\n',''):
-            x.append(c)
-
-        xx = tokenizer.make_data_matrix(x, vocab=self.vocab)
-        pred = self.model.predict(xx)
-        tokenizer.data_matrix_to_conllu(xx, pred, self.vocab, f=buff)
-        
-        return buff.getvalue()
-
+        return tokenizer.tokenize_text(txt,self.model,self.vocab)
+    
 def launch(args,q_in,q_out):
-    global tf, set_session, clear_session,load_model, tokenizer
-    import tensorflow as tf
-    from keras.backend.tensorflow_backend import set_session, clear_session
-    config = tf.ConfigProto()
-    #config.gpu_options.per_process_gpu_memory_fraction = -1
-    config.gpu_options.allow_growth = True
-    set_session(tf.Session(config=config))
-    from keras.models import load_model
+    global tokenizer #imports have to happen within launch() after fork()
     from tokenizer import tokenizer
     
     t=TokenizerWrapper(args)
@@ -51,6 +28,6 @@ def launch(args,q_in,q_out):
         q_out.put((jobid,t.parse_text(txt)))
     
 argparser = argparse.ArgumentParser(description='Tokenize text')
-argparser.add_argument("--model", help="model file")
-argparser.add_argument("--vocab", help="vocab file")
+argparser.add_argument("--model", default="model", help="model file")
+argparser.add_argument("--vocab", default="vocab.pickle", help="vocab file")
 
