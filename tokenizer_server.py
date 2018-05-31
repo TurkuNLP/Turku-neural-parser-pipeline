e98b388854640730b8c806eb4c4b53df4185f30c
Filip Ginter <ginter@cs.utu.fi>
2018-05-31 14:05:52 +0300

    latest tokenizer

--- tokenizer_server.py
+++ tokenizer_server.py
@@ -1,106 +1,8 @@
 import sys
 import io
 import argparse
-
 import numpy as np
 import pickle
-from keras.models import load_model
-
-
-def data_matrix_to_conllu(x, y, vocab, f=sys.stdout):
-
-    #make a reverse vocabulary
-    rev_vocab = {v:k for k,v in vocab.items()}
-    out = ''
-    cntr = 1
-
-    y = y.argmax(-1)
-
-    current_token = ''
-    current_sent = []
-    sents = []
-
-    def write_conllu(sent, f):
-        
-        for i, t in enumerate(sent):
-            line = [str(i+1), t.lstrip(' ')]
-            line.extend(['_']*7)
-            
-            if i > 0:
-                line[6]='1'
-            else:
-                line[6]='0'
-
-
-            if len(sent)-2 > i:
-                if sent[i+1] == ' ':
-                    line.append('SpaceAfter=False')
-                else:
-                    line.append('_')
-            else:
-                line.append('_')
-            f.write('\t'.join(line)+'\n')
-        f.write('\n')
-
-    for a, line in enumerate(x):
-        for b, char in enumerate(line):
-
-
-
-            current_token += rev_vocab[x[a][b]]
-            
-            if y[a][b] == 1.0:
-                if len(current_token.lstrip(' ')) > 0:
-                    current_sent.append(current_token)
-                current_token = ''
-
-            if b > -1 and y[a][b] == 2.0:
-
-                if len(current_token.lstrip(' ')) > 0:
-                    current_sent.append(current_token)
-                current_token = ''
-
-                write_conllu(current_sent,f)
-                current_sent = []
-                cntr = 1
-    else:
-        if current_sent:
-            write_conllu(current_sent,f)
-            
-def make_data_matrix(x, width = 150, vocab=None):
-
-    x_strides = []
-    y_1_str = []
-    y_2_str = []
-
-    for idx in range(0,len(x), width):
-        x_strides.append(x[idx:idx+width])
-
-    #Build vocab
-    if vocab == None:
-        #
-        vocab = {'<mask>': 0.0}
-
-    #Update vocab
-    '''
-    for k in x:
-        if k not in vocab.keys():
-            #
-            vocab[k] = len(vocab)
-    '''
-
-    #Let's build 
-    xnp = np.zeros((len(x_strides), width))
-
-    ##
-    for a, line in enumerate(x_strides):
-        for b, char in enumerate(line):
-            #
-            xnp[a][b] = vocab[x_strides[a][b]]
-
-
-    return xnp
-
 
 class TokenizerWrapper():
 
@@ -126,13 +28,23 @@ class TokenizerWrapper():
         for c in txt.replace('\n',''):
             x.append(c)
 
-        xx = make_data_matrix(x, vocab=self.vocab)
+        xx = tokenizer.make_data_matrix(x, vocab=self.vocab)
         pred = self.model.predict(xx)
-        data_matrix_to_conllu(xx, pred, self.vocab, f=buff)
+        tokenizer.data_matrix_to_conllu(xx, pred, self.vocab, f=buff)
         
         return buff.getvalue()
 
 def launch(args,q_in,q_out):
+    global tf, set_session, clear_session,load_model, tokenizer
+    import tensorflow as tf
+    from keras.backend.tensorflow_backend import set_session, clear_session
+    config = tf.ConfigProto()
+    #config.gpu_options.per_process_gpu_memory_fraction = -1
+    config.gpu_options.allow_growth = True
+    set_session(tf.Session(config=config))
+    from keras.models import load_model
+    from tokenizer import tokenizer
+    
     t=TokenizerWrapper(args)
     while True:
         jobid,txt=q_in.get()
