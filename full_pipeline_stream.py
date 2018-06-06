fd5d6074fb171c0d4e2e0609c11442656f1b1644
Filip Ginter <ginter@cs.utu.fi>
2018-06-06 19:56:58 +0300

    wtf

--- full_pipeline_stream.py
+++ full_pipeline_stream.py
@@ -1,10 +1,11 @@
-from pipelin import Pipeline
+from pipeline import Pipeline
 import sys
 import select
 import threading
 import os
 import yaml
 import time
+import gzip
 
 def non_blocking_batch(inp,timeout=0.2,batch_lines=10000,wait_for_empty_line=False):
     line_buffer=[]
@@ -14,8 +15,7 @@ def non_blocking_batch(inp,timeout=0.2,batch_lines=10000,wait_for_empty_line=Fal
         if not ready_to_read:
             # Stdin is not ready, yield what we've got, if anything
             if line_buffer:
-                #print("Yielding",len(list(line for line in line_buffer if line.startswith("1\t"))), file=sys.stderr)
-                #sys.stderr.flush()
+                print("Yielding on timeout",len(line_buffer),"lines",file=sys.stderr,flush=True)
                 yield "".join(line_buffer)
                 line_buffer=[]
             continue #next try
@@ -26,8 +26,10 @@ def non_blocking_batch(inp,timeout=0.2,batch_lines=10000,wait_for_empty_line=Fal
             line=inp.readline()
             if not line: #End of file detected
                 if line_buffer:
+                    print("Yielding on end-of-input",len(line_buffer),"lines",file=sys.stderr,flush=True)
                     yield "".join(line_buffer)
-                    return
+                print("End-of-file detected",file=sys.stderr,flush=True)
+                return
             line_buffer.append(line)
             if not line.strip(): #empty line
                 break #our chance to yield
@@ -36,9 +38,27 @@ def non_blocking_batch(inp,timeout=0.2,batch_lines=10000,wait_for_empty_line=Fal
 
         # Now we got the next sentence --- do we have enough to yield?
         if len(line_buffer)>batch_lines:
+            print("Yielding on full batch",len(line_buffer),"lines",file=sys.stderr,flush=True)
             yield "".join(line_buffer) #got enough
             line_buffer=[]
 
+def blocking_batch(inp,batch_lines=10000,wait_for_empty_line=False):
+    line_buffer=[]
+    while True:
+        print("Waiting for next line",file=sys.stderr,flush=True)
+        line=inp.readline()
+        print("Line",repr(line),file=sys.stderr,flush=True)
+        if not line: #EOF
+            if line_buffer:
+                print("Yielding on EOF",file=sys.stderr,flush=True)
+                yield "".join(line_buffer)
+            print("Done batching",file=sys.stderr,flush=True)
+            return
+        line_buffer.append(line)
+        if (line.strip()=="" or not wait_for_empty_line) and len(line_buffer)>batch_lines:
+            print("Yielding",file=sys.stderr,flush=True)
+            yield "".join(line_buffer)
+            line_buffer=[]
 
 def output_thread(p,out,verbose=False):
     started=time.time()
@@ -67,6 +87,8 @@ if __name__=="__main__":
     argparser.add_argument('--conf-yaml', default=os.path.join(THISDIR,"pipelines.yaml"), help='YAML with pipeline configs. Default: parser_dir/pipelines.yaml')
     argparser.add_argument('--pipeline', default="fi_tdt_all", help='Name of the pipeline to run, one of those given in the YAML file. Default: %(default)s')
     argparser.add_argument('--empty-line-batching', default=False, action="store_true", help='Only ever batch on newlines (useful with pipelines that input conllu)')
+    argparser.add_argument('--batch-lines', default=10000, help='Number of lines in a job batch. Default %(default)d')
+    argparser.add_argument('--blocking-read', default=None, help='Use blocking read instead of non-blocking, give a filename (can be gzip)')
     args = argparser.parse_args()
 
     with open(args.conf_yaml) as f:
@@ -80,8 +102,25 @@ if __name__=="__main__":
     #Fetch results from pipeline in an infinite loop
     outp_t=threading.Thread(target=output_thread,args=(p,sys.stdout,True))
     outp_t.start()
+    
+    time.sleep(3)
 
-    for batch in non_blocking_batch(sys.stdin,wait_for_empty_line=args.empty_line_batching):
+    if args.blocking_read:
+        fname=args.blocking_read
+        print("Blocking batches from",fname,file=sys.stderr,flush=True)
+        if fname.endswith(".gz"):
+            f=gzip.open(fname,"rt")
+        else:
+            f=open(fname)
+        print("Preparing blocking batches",fname,f,file=sys.stderr,flush=True)
+        batches=blocking_batch(f,batch_lines=args.batch_lines,wait_for_empty_line=args.empty_line_batching)
+        print("Starting input batching (blocking)...",batches,file=sys.stderr,flush=True)
+    else:
+        print("Preparing nonblocking batches",fname,f,file=sys.stderr,flush=True)
+        batches=non_blocking_batch(sys.stdin,batch_lines=args.batch_lines,wait_for_empty_line=args.empty_line_batching)
+        print("Starting input batching (non-blocking)...",file=sys.stderr,flush=True)
+    for batch in batches:
+        print("\n\nSubmitting a batch of ",batch.count("\n"),"lines\n\n",file=sys.stderr,flush=True)
         p.put(batch)
         assert outp_t.is_alive()
     p.input_finished=True
