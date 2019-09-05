1ad96c32cfbc34007c1b42ed0784bf2d0d4e1fe2
Jenna Kanerva <jmnybl@utu.fi>
2019-09-05 10:30:43 +0300

    report also tokens per second

--- output_mod.py
+++ output_mod.py
@@ -2,21 +2,25 @@ import sys
 import io
 import argparse
 import time
+import re
 
+token_regex=re.compile("[0-9]+\t")
 
 def launch(args,q_in,q_out):
     start=time.time()
-    total_parsed=0
+    total_parsed_trees=0
+    total_parsed_tokens=0
     next_report=start+10.0 #report every 10sec at most
     while True:
         jobid,txt=q_in.get()
         if jobid=="FINAL":
             print("Output exiting",file=sys.stderr,flush=True)
             return
-        total_parsed+=sum(1 for line in txt.split("\n") if line.startswith("1\t"))
-        if total_parsed>0 and time.time()>next_report:
+        total_parsed_trees+=sum(1 for line in txt.split("\n") if line.startswith("1\t"))
+        total_parsed_tokens+=sum(1 for line in txt.split("\n") if re.match(token_regex, line))
+        if total_parsed_trees>0 and time.time()>next_report:
             time_spent=time.time()-start
-            print("Runtime: {}:{} [m:s]  Parsed: {} [trees]  Speed: {} [trees/sec]  {} [sec/tree]".format(int(time_spent)//60,int(time_spent)%60,total_parsed,total_parsed/time_spent,time_spent/total_parsed),file=sys.stderr,flush=True)
+            print("Runtime: {}:{} [m:s]  Parsed: {} [trees], {} [tokens]  Speed: {} [trees/sec]  {} [sec/tree] {} [tokens/sec]".format(int(time_spent)//60,int(time_spent)%60,total_parsed_trees,total_parsed_tokens, total_parsed_trees/time_spent,time_spent/total_parsed_trees, total_parsed_tokens/time_spent) ,file=sys.stderr,flush=True)
             next_report=time.time()+10
         print(txt,end="",flush=True)
     
